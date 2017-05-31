9cd79e3f3aee13c4fa0ffaf22b89c65646d7c7ba
Xiodi <gwendal.patat@gmail.com>
2017-05-31 17:36:46 +0200

    Update of the Script and real Rotation in RotN.pm

--- RotN.pm
+++ RotN.pm
@@ -8,18 +8,23 @@ our @EXPORT = qw(&rotString);
 
 sub rotN
 {
-    my ($char, $n, $operator) = @_;
-    return chr(ord($char) + $n) if ($operator eq "ENCRYPT");
-    return chr(ord($char) - $n) if ($operator eq "DECRYPT");
+    my ($char, $n) = @_;
+    my $op = ($n > 0 ? -26 : 26);
+    if (chr(ord($char) + $n) =~ /[^a-zA-Z]/)
+    {
+	return chr(ord($char) + $n + $op) if (chr(ord($char) + $n + $op) =~ /[a-zA-Z]/);
+	return $char;
+    }
+    return chr(ord($char) + $n);
 }
 
 sub rotString
 {
-    my ($string, $n, $operator) = @_;
+    my ($string, $n) = @_;
     my $crypString = "";
     foreach (split //, $string) 
     {
-	$crypString .= ($_ =~ /\S/ ? rotN($_, $n, $operator) : $_);
+	$crypString .= ($_ =~ /\S/ ? rotN($_, $n) : $_);
     }
     return $crypString;
 }
--- rotScript.pl
+++ rotScript.pl
@@ -1,9 +1,17 @@
 #!/usr/bin/perl
-
 use strict;
 use warnings;
+use Scalar::Util qw(looks_like_number);
 use RotN;
 
-my $before = <>;
-my $after = rotString($before, 3, "ENCRYPT");
-print("After encryption : ".$after."And after decrypt : " . rotString($after, 3, "DECRYPT"));
+die ("Usage: $0 <1|2> <n> <nameOfFile>\n") if (!defined($ARGV[2]));
+die ("Usage : $ARGV[0] must be 1 (Encrypt) or 2 (Decrypt)") if ( $ARGV[0] != 1 && $ARGV[0] != 2);
+die ("Usage: $ARGV[1] is not an integer\n") if (!looks_like_number($ARGV[1]));
+open(my $sourceFile, '<', $ARGV[2]) or die("open: $!");
+my $number = $ARGV[1];
+$number = abs($number) % 26;
+$number = -$number if ($ARGV[0] == 2);
+while (defined(my $c = <$sourceFile>))
+{
+    print(rotString($c, $number));
+}
