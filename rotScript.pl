385fee2289f8208228da17b77fea326fd310927d
Xiodi <gwendal.patat@gmail.com>
2017-05-31 17:48:04 +0200

    Modification of the Usage

--- rotScript.pl
+++ rotScript.pl
@@ -4,7 +4,7 @@ use warnings;
 use Scalar::Util qw(looks_like_number);
 use RotN;
 
-die ("Usage: $0 <1|2> <n> <nameOfFile>\n") if (!defined($ARGV[2]));
+die ("Usage: $0 {1|2} n sourceFile\n") if (!defined($ARGV[2]));
 die ("Usage : $ARGV[0] must be 1 (Encrypt) or 2 (Decrypt)") if ( $ARGV[0] != 1 && $ARGV[0] != 2);
 die ("Usage: $ARGV[1] is not an integer\n") if (!looks_like_number($ARGV[1]));
 open(my $sourceFile, '<', $ARGV[2]) or die("open: $!");
